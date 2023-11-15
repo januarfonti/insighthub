5c67dc619ec0b66d1fcfc55cd9550a1a1535dd7f
Januar Fonti <januar@fonti.dev>
2023-11-15 21:55:45 +0700

    Add FAQ page

--- components/Header.vue
+++ components/Header.vue
@@ -4,11 +4,11 @@
     <div class="container flex items-center justify-between ">
       <NuxtLink to="/" class="font-bold text-white hover:text-sky-600 transition">InsightHub</NuxtLink>
       <nav class="hidden md:flex items-center gap-10 text-sm">
-        <NuxtLink to="#fitur" class="hover:text-white transition">Tentang</NuxtLink>
+        <NuxtLink to="#tentang" class="hover:text-white transition">Tentang</NuxtLink>
         <NuxtLink to="#fitur" class="hover:text-white transition">Fitur</NuxtLink>
         <NuxtLink to="/faq" class="hover:text-white transition">FAQ</NuxtLink>
         <NuxtLink to="/dokumentasi" class="hover:text-white transition">Dokumentasi</NuxtLink>
-        <NuxtLink to="/login" class="px-6 py-2 rounded-lg bg-sky-600 hover:bg-sky-400 transition text-white font-semibold">Isi Survey</NuxtLink>
+        <NuxtLink to="/login" class="px-6 py-2 rounded-lg bg-sky-600 hover:bg-sky-400 transition text-white font-semibold">Isi Survei</NuxtLink>
       </nav>
       <button class="md:hidden">
         <svg class="w-6 h-6 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
