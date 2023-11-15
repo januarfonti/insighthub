cc115db38fd8d4920df0e2d7ca16ea9dc77abfb2
Januar Fonti <januar@fonti.dev>
2023-11-15 19:34:48 +0700

    Refactor Tentant Section

--- components/section/Tentang.vue
+++ components/section/Tentang.vue
@@ -5,17 +5,9 @@
       <p class="max-w-2xl mx-auto text-center">Sebuah aplikasi survei kepuasan yang dapat digunakan oleh civitas akademika fakultas Vokasi untuk mengukur kepuasan mahasiswa, tenaga kependidikan, dan dosen fakultas Vokasi Universitas Brawijaya.</p>
       <div class="flex gap-8 md:gap-10 py-8 md:py-20 md:flex-row flex-col-reverse">
         <div class="w-full md:w-2/3 space-y-4">
-          <div class="p-4 rounded space-y-2 border border-slate-800 hover:bg-slate-800 transition">
-            <h2 class="text-lg md:text-xl text-white font-bold">Mengukur Kepuasan Mahasiswa</h2>
-            <p>Memungkinkan mahasiswa memberikan umpan balik terkait pengalaman akademik, dengan analisis hasil survei untuk perbaikan layanan pendidikan dan lingkungan kampus. Tujuannya adalah meningkatkan kualitas pendidikan, termasuk kurikulum, fasilitas, dan dukungan mahasiswa.</p>
-          </div>
-          <div class="p-4 rounded space-y-2 border border-slate-800 hover:bg-slate-800 transition">
-            <h2 class="text-xl text-white font-bold">Mengukur Kepuasan Tenaga Kependidikan</h2>
-            <p>Survei khusus untuk mengevaluasi kinerja dan sistem pelayanan tenaga kependidikan, mendeteksi masalah yang perlu diperbaiki untuk menjaga hubungan positif di lingkungan kerja.</p>
-          </div>
-          <div class="p-4 rounded space-y-2 border border-slate-800 hover:bg-slate-800 transition">
-            <h2 class="text-xl text-white font-bold">Mengukur Kepuasan Dosen</h2>
-            <p>Mekanisme survei khusus untuk evaluasi kualitas lulusan dan kolaborasi dengan mitra industri. Hasil survei digunakan untuk meningkatkan kualitas kurikulum dan pelatihan yang relevan bagi civitas akademik.</p>
+          <div v-for="(item, index) in items" :key="index" class="p-4 rounded space-y-2 border border-slate-800 hover:bg-slate-800 transition">
+            <h2 class="text-lg md:text-xl text-white font-bold">{{ item.title }}</h2>
+            <p>{{ item.description }}</p>
           </div>
         </div>
         <div class="w-full md:w-1/2">
@@ -25,3 +17,20 @@
     </div>
   </div>
 </template>
+
+<script setup lang="ts">
+const items = [
+  {
+    title: 'Mengukur Kepuasan Mahasiswa',
+    description: 'Memungkinkan mahasiswa memberikan umpan balik terkait pengalaman akademik, dengan analisis hasil survei untuk perbaikan layanan pendidikan dan lingkungan kampus. Tujuannya adalah meningkatkan kualitas pendidikan, termasuk kurikulum, fasilitas, dan dukungan mahasiswa.'
+  },
+  {
+    title: 'Mengukur Kepuasan Tenaga Kependidikan',
+    description: 'Survei khusus untuk mengevaluasi kinerja dan sistem pelayanan tenaga kependidikan, mendeteksi masalah yang perlu diperbaiki untuk menjaga hubungan positif di lingkungan kerja.'
+  },
+  {
+    title: 'Mengukur Kepuasan Dosen',
+    description: 'Mekanisme survei khusus untuk evaluasi kualitas lulusan dan kolaborasi dengan mitra industri. Hasil survei digunakan untuk meningkatkan kualitas kurikulum dan pelatihan yang relevan bagi civitas akademik.'
+  }
+]
+</script>
