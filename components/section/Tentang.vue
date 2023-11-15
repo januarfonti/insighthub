57efb5e91f85cb644e68ff9b6c4b5d13ceae5c4d
Januar Fonti <januar@fonti.dev>
2023-11-15 19:28:42 +0700

    Add Tentang section

---
+++ components/section/Tentang.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="py-10 md:py-20">
+    <div class="container space-y-4">
+      <h1 class="text-white text-xl md:text-2xl font-bold text-center">Tentang InsightHub</h1>
+      <p class="max-w-2xl mx-auto text-center">Sebuah aplikasi survei kepuasan yang dapat digunakan oleh civitas akademika fakultas Vokasi untuk mengukur kepuasan mahasiswa, tenaga kependidikan, dan dosen fakultas Vokasi Universitas Brawijaya.</p>
+      <div class="flex gap-8 md:gap-10 py-8 md:py-20 md:flex-row flex-col-reverse">
+        <div class="w-full md:w-2/3 space-y-4">
+          <div class="p-4 rounded space-y-2 border border-slate-800 hover:bg-slate-800 transition">
+            <h2 class="text-lg md:text-xl text-white font-bold">Mengukur Kepuasan Mahasiswa</h2>
+            <p>Memungkinkan mahasiswa memberikan umpan balik terkait pengalaman akademik, dengan analisis hasil survei untuk perbaikan layanan pendidikan dan lingkungan kampus. Tujuannya adalah meningkatkan kualitas pendidikan, termasuk kurikulum, fasilitas, dan dukungan mahasiswa.</p>
+          </div>
+          <div class="p-4 rounded space-y-2 border border-slate-800 hover:bg-slate-800 transition">
+            <h2 class="text-xl text-white font-bold">Mengukur Kepuasan Tenaga Kependidikan</h2>
+            <p>Survei khusus untuk mengevaluasi kinerja dan sistem pelayanan tenaga kependidikan, mendeteksi masalah yang perlu diperbaiki untuk menjaga hubungan positif di lingkungan kerja.</p>
+          </div>
+          <div class="p-4 rounded space-y-2 border border-slate-800 hover:bg-slate-800 transition">
+            <h2 class="text-xl text-white font-bold">Mengukur Kepuasan Dosen</h2>
+            <p>Mekanisme survei khusus untuk evaluasi kualitas lulusan dan kolaborasi dengan mitra industri. Hasil survei digunakan untuk meningkatkan kualitas kurikulum dan pelatihan yang relevan bagi civitas akademik.</p>
+          </div>
+        </div>
+        <div class="w-full md:w-1/2">
+          <img src="~/assets/images/preview.png" alt="Preview" class="rounded-lg shadow-lg" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
     <SectionHero />
+    <SectionTentang />
   </div>
 </template>
