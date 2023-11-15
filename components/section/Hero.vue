7d6e1b6e3ce8fc307143c4d8a1b7b537f2d905f5
Januar Fonti <januar@fonti.dev>
2023-11-15 16:28:18 +0700

    Add hero section component

---
+++ components/section/Hero.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="py-20">
+    <div class="container">
+      <div class="flex flex-col items-center justify-center gap-10">
+        <span class="px-3 py-1 bg-slate-600 text-white rounded-full text-xs">Wujudkan Pemahaman, Bentuk Masa Depan</span>
+        <h1 class="text-6xl font-bold text-center text-white max-w-5xl">InsightHub: Wawasan untuk Keputusan, Perubahan Positif.</h1>
+        <p class="text-center max-w-5xl text-xl">InsightHub adalah platform untuk mengumpulkan data dari responden yang dapat digunakan untuk keperluan riset, survei, dan lainnya. Dengan InsightHub, Anda dapat mengumpulkan data dari responden dengan mudah dan cepat.</p>
+        <div class="gap-4 flex flex-wrap">
+          <NuxtLink to="/login" class="px-6 h-11 flex whitespace-nowrap items-center text-sm rounded-lg bg-white hover:bg-sky-400 transition text-slate-900 font-semibold">Pelajari Lebih Lanjut</NuxtLink>
+          <NuxtLink to="/register" class="px-6 h-11 flex whitespace-nowrap items-center text-sm rounded-lg  transition text-white hover:bg-white hover:text-slate-900 font-semibold border border-slate-600">Mulai Isi Survei</NuxtLink>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
